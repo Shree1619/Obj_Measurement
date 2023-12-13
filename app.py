6483f54befa19dacf2cfb98e2c94bdfb67159c8f
Shrikant Mundhe <110709002+Shree1619@users.noreply.github.com>
2023-12-14 00:00:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def run_camera():
     wP = 210  # Replace with the actual width of the object to measure
     hP = 297  # Replace with the actual height of the object to measure
 
-    cap = cv2.VideoCapture(2)
+    cap = cv2.VideoCapture(3)
     cap.set(10, 160)
     cap.set(3, 1280)
     cap.set(4, 720)
