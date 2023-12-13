ae4afe8b29b12b16315ffc1f7b8e9fc040f7e9c6
Shrikant Mundhe <110709002+Shree1619@users.noreply.github.com>
2023-12-13 19:43:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def measure_object(image, wP, hP):
             for obj in conts2:
                 cv2.polylines(imgContours2, [obj[2]], True, (0, 255, 0), 1)
                 nPoints = utlis.reorder(obj[2])
-                nW = round((utlis.findDis(nPoints[0][0] // 3, nPoints[1][0] // 3) / 10), 1)
+                nW = round((utlis.findDis(nPoints[0][0] // 1, nPoints[1][0] // 1) / 10), 1)
                 nH = round((utlis.findDis(nPoints[0][0] // 0.5, nPoints[2][0] // 0.5) / 10), 1)
                 cv2.arrowedLine(imgContours2, (nPoints[0][0][0], nPoints[0][0][1]),
                                 (nPoints[1][0][0], nPoints[1][0][1]), (0, 0, 255), 1, 8, 0, 0.05)
